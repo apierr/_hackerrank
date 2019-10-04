47d969d74168cda3e414586a9415da371697ab09
apierro <antonio.pierro@gmail.com>
2019-10-04 16:12:43 +0200

    added removeDuplicates from linkedList

--- src/30-days-of-code/30-linked-list-deletion.js
+++ src/30-days-of-code/30-linked-list-deletion.js
@@ -1,21 +1,6 @@
 process.stdin.resume();
 process.stdin.setEncoding("ascii");
 
-var input_stdin = "";
-var input_stdin_array = "";
-var input_currentline = 0;
-
-process.stdin.on("data", function(data) {
-    input_stdin += data;
-});
-
-process.stdin.on("end", function() {
-    input_stdin_array = input_stdin.split("\n");
-    main();
-});
-function readLine() {
-    return input_stdin_array[input_currentline++];
-}
 function Node(data) {
     this.data = data;
     this.next = null;
@@ -23,6 +8,15 @@ function Node(data) {
 function Solution() {
     this.removeDuplicates = function(head) {
         //Write your code here
+        let current = head;
+        while (current.next) {
+            if (current.data == current.next.data) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+        return head;
     };
 
     this.insert = function(head, data) {
@@ -49,14 +43,18 @@ function Solution() {
         }
     };
 }
-function main() {
-    var T = parseInt(readLine());
+function main(str) {
+    input = str.split(" ").map(Number);
+    const T = parseInt(input.splice(0, 1));
     var head = null;
     var mylist = new Solution();
+
     for (i = 0; i < T; i++) {
-        var data = parseInt(readLine());
+        var data = input[i];
         head = mylist.insert(head, data);
     }
     head = mylist.removeDuplicates(head);
     mylist.display(head);
 }
+
+main("6 1 2 2 3 3 4");
