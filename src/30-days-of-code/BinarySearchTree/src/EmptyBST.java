3f9129753d7d26d673837fc401ea4fe5ac6f1422
apierro <antonio.pierro@gmail.com>
2019-10-04 06:47:47 +0200

    updating BinarySearchTree EmptyBST and NonEmptyBST

--- src/30-days-of-code/BinarySearchTree/src/EmptyBST.java
+++ src/30-days-of-code/BinarySearchTree/src/EmptyBST.java
@@ -1,4 +1,4 @@
-/*
+ /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -8,6 +8,30 @@
  *
  * @author antonio
  */
-public class EmptyBST {
+public class EmptyBST<D extends Comparable> implements Tree<D>{
+    
+    public EmptyBST() {
+        
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return true;
+    }
+
+    @Override
+    public int cardinality() {
+        return 0;
+    }
+
+    @Override
+    public boolean member(D elt) {
+        return false;
+    }
+
+    @Override
+    public NonEmptyBST<D> add(D elt) {
+        return new NonEmptyBST<D>(elt);
+    }
     
 }
--- src/30-days-of-code/BinarySearchTree/src/NonEmptyBST.java
+++ src/30-days-of-code/BinarySearchTree/src/NonEmptyBST.java
@@ -8,6 +8,42 @@
  *
  * @author antonio
  */
-class NonEmptyBST<T> {
+class NonEmptyBST<D extends Comparable> implements Tree<D>{
+    D data;
+    Tree<D> left;
+    Tree<D> right;
     
+    public NonEmptyBST(D elt) {
+        data = elt;
+        left = new EmptyBST<D>();
+        right = new EmptyBST<D>();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int cardinality() {
+        return 1 + left.cardinality() + right.cardinality();
+    }
+
+    @Override
+    public boolean member(D elt) {
+        if (data == elt) {
+            return true;
+        } else {
+            if (elt < data) {
+                return left.member(elt);
+            } else {
+                return right.member(elt);
+            }
+        }
+    }
+
+    @Override
+    public NonEmptyBST<D> add(D elt) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
