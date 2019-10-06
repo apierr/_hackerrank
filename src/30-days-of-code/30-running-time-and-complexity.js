f04974b5ccec729a21e081c591c075ea5157d88e
apierro <antonio.pierro@gmail.com>
2019-10-06 11:40:13 +0200

    Added isPrime method in js

--- src/30-days-of-code/30-running-time-and-complexity.js
+++ src/30-days-of-code/30-running-time-and-complexity.js
@@ -1,14 +1,24 @@
 function processData(input) {
-    //Enter your code here
+    const numbers = input.split("\n").map(Number);
+    numbers.shift();
+
+    numbers.forEach(element => {
+        console.log(isPrime(element) ? "Prime" : "Not Prime");
+    });
 }
 
-process.stdin.resume();
-process.stdin.setEncoding("ascii");
-_input = "";
-process.stdin.on("data", function(input) {
-    _input += input;
-});
+function isPrime(num) {
+    if (num === 1) return false;
+    for (i = 2; i * i <= num; i++) {
+        if (num % i === 0) return false;
+    }
+    return true;
+}
+function main() {
+    const input = `2
+    31
+    33`;
+    processData(input);
+}
 
-process.stdin.on("end", function() {
-    processData(_input);
-});
+main();
