3451a192df311944f7be899fdb373fbc19d5b731
apierro <antonio.pierro@gmail.com>
2019-10-23 18:20:35 +0200

    Added print-the-elements-of-a-linked-list.js

---
+++ src/data-structures/linked-lists/print-the-elements-of-a-linked-list.js
@@ -0,0 +1,59 @@
+// The first line of input contains , the number of elements in the linked list.
+// The next  lines contain one element each, which are the elements of the linked list.
+
+const SinglyLinkedListNode = class {
+    constructor(nodeData) {
+        this.data = nodeData;
+        this.next = null;
+    }
+};
+
+const SinglyLinkedList = class {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    insertNode(nodeData) {
+        const node = new SinglyLinkedListNode(nodeData);
+
+        if (this.head == null) {
+            this.head = node;
+        } else {
+            this.tail.next = node;
+        }
+
+        this.tail = node;
+    }
+};
+
+// Complete the printLinkedList function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode next;
+ * }
+ *
+ */
+function printLinkedList(head) {
+    if (head != null) {
+        console.log(head.data);
+        printLinkedList(head.next);
+    }
+}
+
+function main() {
+    let llist = new SinglyLinkedList();
+
+    listItems = [17, 19, 5, 12];
+    listItems.forEach(e => {
+        llist.insertNode(e);
+    });
+
+    printLinkedList(llist.head); // ->17, 19, 5, 12
+}
+
+main();
