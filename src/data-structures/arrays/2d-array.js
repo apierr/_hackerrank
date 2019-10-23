820dcb9eb79f23abdfb5389e7f6a289a9b19bd61
apierro <antonio.pierro@gmail.com>
2019-10-23 17:37:38 +0200

    Added 2d-array.js

--- src/data-structures/arrays/2d-array.js
+++ src/data-structures/arrays/2d-array.js
@@ -1,5 +1,19 @@
 // Complete the hourglassSum function below.
-function hourglassSum(arr) {}
+function hourglassSum(arr) {
+    let max = -Infinity;
+    for (let i = 0; i < 4; i++) {
+        for (let j = 0; j < 4; j++) {
+            const nums = [
+                ...arr[i].slice(j, j + 3),
+                arr[i + 1][j + 1],
+                ...arr[i + 2].slice(j, j + 3)
+            ];
+            sum = nums.reduce((c, a) => c + a, 0);
+            if (max < sum) max = sum;
+        }
+    }
+    return max;
+}
 
 function main() {
     const arr = [
@@ -13,3 +27,5 @@ function main() {
 
     console.log(hourglassSum(arr)); // 19
 }
+
+main();
